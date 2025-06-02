190b0118d702925bb42d91104f3fcb237df75bdd
Adam Perkins <aperkins@equip.health>
2025-06-02 09:37:09 -0400

    Provide http client and common module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 const API_URL = "https://randomuser.me/api/?results=15";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
